0f606a7fb6eb53dfa952417ee644cb12985aad05
Diabetaco <loganback@att.net>
2022-07-11 00:41:47 -0400

    added script

---
+++ PracticeTreeGame/Assets/Scripts/LED_Controller.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LED_Controller : MonoBehaviour
+{
+    public bool middleBulb; //idk name
+    public bool prestage;
+    public bool stage;
+    public bool top;
+    public bool middle;
+    public bool bottom;
+    public bool green;
+    public bool red;
+
+    public float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //logic
+        //pro tree
+        //full tree
+        //4 tenths
+        //5 tenths
+    }
+}
